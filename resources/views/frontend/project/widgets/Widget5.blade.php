eaff904fc52d3ce02dc163f57696a205fc6f76d6
rad4uk <yurij.radchuk@mail.ru>
2023-04-17 17:16:29 +0300

    fix text widget 5

--- resources/views/frontend/project/widgets/Widget5.blade.php
+++ resources/views/frontend/project/widgets/Widget5.blade.php
@@ -5,12 +5,12 @@
                 <div class="project-widget5__container">
                     <img src="{{asset('/storage/images/project/' . $widget->file)}}" alt="" class="project-widget5__container-img">
                 </div>
+                @if(isset($widget->text) && mb_strlen($widget->text) > 0)
+                    <p class="project-widget5-text">
+                        {{$widget->text}}
+                    </p>
+                @endif
             </div>
         </div>
-        @if(isset($widget->text) && mb_strlen($widget->text) > 0)
-            <p class="project-widget5-text">
-                {{$widget->text}}
-            </p>
-        @endif
     </div>
 @endif
