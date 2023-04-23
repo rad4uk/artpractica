edcf47cf12257da8e58a5428854489ec945ac474
rad4uk <yurij.radchuk@mail.ru>
2023-04-23 22:45:53 +0300

    fix services

--- resources/js/Components/frontend/design/firstTemplate.vue
+++ resources/js/Components/frontend/design/firstTemplate.vue
@@ -1,121 +1,61 @@
 <template>
-<!--    <div>-->
-<!--        <slot name="designTextComponent"></slot>-->
+    <!--    <div>-->
+    <!--        <slot name="designTextComponent"></slot>-->
 
     <div class="designtext">
-            <div class="container">
-                <div class="designtext__row">
-                    <div class="designtext__title">
-                        <slot name="title">
-                            <h1>ДИЗАЙН ПРОЕКТИРОВАНИЕ</h1>
-                        </slot>
-                    </div>
-
-                    <div class="designtext__text">
-                        <slot name="text">
-                            <p>
-                                Помните русскую пословицу «Не место красит человека, а человек – место»?
-                            </p>
-                        </slot>
-                        <slot name="text">
-                            <p>
-                                Проработав с профессионалами своего дела, людьми, объединенными вот уже более 15 лет общей специальностью, креативным мышлением, творческим началом, готовы заверить: такой принцип может быть зеркален.
-                            </p>
-                        </slot>
-                        <slot name="text">
-                            <p>
-                                История ArtPractica о том, как место повлияло на жизни многих людей. Как были задуманы и воплощены сотни красивых и функциональных проектов, соединивших владельцев с их мечтами.
-                            </p>
-                        </slot>
-                        <slot name="text">
-                            <p>
-                                Наша команда предлагает индивидуальный сервис для создания дизайн-проекта любой сложности, формирования образа дома, который вы хотите полюбить. И мы готовы приложить все усилия, чтобы это была любовь с первого взгляда.
-                            </p>
-                        </slot>
-                    </div>
-
-                    <div class="sectionimage__block">
-                        <div class="sectionimage__image">
-                            <img src="../../../../../public/images/design/img.png" alt="">
-                        </div>
-                    </div>
-                    <a href="#form" class="designtext__button">ОСТАВИТЬ ЗАЯВКУ</a>
+        <div class="container">
+            <div class="designtext__row">
+                <div class="designtext__title">
+                    <slot name="title">
+                        <h1>{{ this.data.title }}</h1>
+                    </slot>
                 </div>
+
+                <div class="designtext__text" v-html="this.data.description">
+                </div>
+
+                <div class="sectionimage__block">
+                    <div class="sectionimage__image">
+                        <img :src="this.data.preview_image" :alt="this.data.title">
+                    </div>
+                </div>
+                <a href="#form" class="designtext__button">ОСТАВИТЬ ЗАЯВКУ</a>
             </div>
         </div>
+    </div>
 
-    <div class="stages">
+    <div class="stages" v-if="this.data.first_section_description.length > 0">
         <div class="container">
             <div class="stages__title">
                 <h3>ЭТАПЫ РАБОТЫ НАД ДИЗАЙН ПРОЕКТОМ</h3>
             </div>
             <div class="row">
                 <div class="stages__column">
-                    <div class="stages__column-item stages-item">
+                    <div
+                        v-for="(item, idx) in this.first_column_data"
+                        :key="idx"
+                        class="stages__column-item stages-item">
                         <div class="stages-item-title">
 
-                            <p><span>01</span>встреча с заказчиком</p>
+                            <p><span>0{{ idx + 1 }}</span>{{ item.input }}</p>
                         </div>
                         <div class="stages-item-content">
-                            <p>Обсуждение дизайнера и заказчика его идей и пожеланий.</p>
+                            <p>{{ item.textarea }}</p>
                         </div>
                     </div>
-                    <div class="stages__column-item stages-item">
-                        <div class="stages-item-title">
-                            <p><span>02</span>ЗАКЛЮЧЕНИЕ ДОГОВОРА</p>
-                        </div>
-                        <div class="stages-item-content">
-                            <p>Заключение договора с определением четких сроков, состава проекта и индивидуальных условий.</p>
-                        </div>
-                    </div>
-                    <div class="stages__column-item stages-item">
-                        <div class="stages-item-title">
-                            <p><span>03</span>РАЗРАБОТКА КОНЦЕПЦИИ</p>
-                        </div>
-                        <div class="stages-item-content">
-                            <p>Определение основной идеи проекта: стиль, цветовые решения, материалы, настроение.</p>
-                        </div>
-                    </div>
-                    <div class="stages__column-item stages-item">
-                        <div class="stages-item-title">
-                            <p><span>04</span>СОЗДАНИЕ ВИЗУАЛИЗАЦИИ</p>
-                        </div>
-                        <div class="stages-item-content">
-                            <p>Демонстрация 3D – изображений, реально отражающих каждый уголок вашего будущего пространства.</p>
-                        </div>
-                    </div>
+
                 </div>
-                <div class="stages__column">
-                    <div class="stages__column-item stages-item">
+
+                <div class="stages__column" v-if="this.second_column_data.length > 0">
+                    <div
+                        v-for="(item, idx) in this.second_column_data"
+                        :key="idx"
+                        class="stages__column-item stages-item">
                         <div class="stages-item-title">
-                            <p><span>05</span>ВЫЕЗД НА ОБМЕРЫ</p>
+                            <p><span>0{{ this.first_column_data_length + idx }}</span>{{ item.input }}</p>
                         </div>
                         <div class="stages-item-content">
-                            <p>Выезд архитектора-дизайнера на профессиональный обмер в любое удобное для заказчика время без географических ограничений</p>
-                        </div>
-                    </div>
-                    <div class="stages__column-item stages-item">
-                        <div class="stages-item-title">
-                            <p><span>06</span>ПРОЕКТНАЯ ДОКУМЕНТАЦИЯ</p>
-                        </div>
-                        <div class="stages-item-content">
-                            <p>Трансформация 3D-изображений в конкретные чертежи, позволяющие воплотить идеи архитектора-дизайнера в жизнь.</p>
-                        </div>
-                    </div>
-                    <div class="stages__column-item stages-item">
-                        <div class="stages-item-title">
-                            <p><span>07</span>ПЛАНИРОВОЧНОЕ РЕШЕНИЕ</p>
-                        </div>
-                        <div class="stages-item-content">
-                            <p>Профессиональная разработка грамотного планировочного решения – гарант успеха будущего проекта.</p>
-                        </div>
-                    </div>
-                    <div class="stages__column-item stages-item">
-                        <div class="stages-item-title">
-                            <p><span>08</span>ВОПЛОЩЕНИЕ</p>
-                        </div>
-                        <div class="stages-item-content">
-                            <p>На основании согласованного с заказчиком проекта команда ArtPractica сделает любую идею реальной независимо от ее сложности.</p>
+                            <p>{{ item.textarea }}</p>
                         </div>
                     </div>
                 </div>
@@ -128,6 +68,22 @@
 export default {
     name: "firstTemplate",
     props: ['data'],
+    data: () => {
+        return {
+            first_column_data_length: 0,
+            first_column_data: [],
+            second_column_data: [],
+        }
+    },
+    mounted() {
+        if (this.data.first_section_description.length > 0) {
+            const length = this.data.first_section_description.length;
+            const part = Math.ceil(length / 2);
+            this.first_column_data = this.data.first_section_description.slice(0, part); // первые 6 элементов
+            this.second_column_data = this.data.first_section_description.slice(length - part); // оставшиеся 5 элементов
+            this.first_column_data_length = part + 1;
+        }
+    }
 }
 </script>
 
--- resources/js/Components/frontend/design/thirdTemplate.vue
+++ resources/js/Components/frontend/design/thirdTemplate.vue
@@ -3,19 +3,13 @@
         <div class="atelier-furniture">
             <div class="container">
                 <div class="atelier-furniture-title service-title">
-                    ARTPRACTICA FURNITURE ATELIER
+                   {{this.data.title}}
                 </div>
-                <div class="atelier-furniture-text">
-                    <p class="service-text">
-                        МЕБЕЛЬНОЕ ПРОИЗВОДСТВО <a href="https://apfurniture.ru/" target="_blank">APFURNITURE.RU</a>
-                    </p>
-                    <p class="service-text">
-                        Работает с 2017 года. На площади более 1500 кв.м. расположено современное оборудование для выпуска эксклюзивных дизайнерских изделий. Стаж каждого сотрудника - не менее 10 лет. Мы создаем мебель любой сложности от минимализма до классики. Уверены, что качество всегда будет в тренде. Для нас важно, чтобы изделия выглядели эстетично и радовали вас десятилетиями.
+                <div class="atelier-furniture-text" v-html="this.data.description">
 
-                    </p>
                 </div>
                 <div class="atelier-furniture-image">
-                    <img src="../../../../../public/images/design/img_3.png" alt="">
+                    <img :src="this.data.preview_image" :alt="this.data.title">
                 </div>
             </div>
         </div>
@@ -23,12 +17,14 @@
         <div class="atelier-team">
             <div class="container">
                 <div class="atelier-team-title service-title">
-                    КОМАНДА ПРОФЕССИОНАЛОВ
+                    {{this.data.second_section_title}}
                 </div>
                 <div class="atelier-team-text">
-                    <p class="service-text">
-                        <span>ARTPRACTICA FURNITURE — </span>это опытная команда профессионалов и высокотехнологичное производство полного цикла. Успешно воплощаем идеи дизайнеров. Индивидуальный подход к каждому проекту.
+                    <p class="service-text" v-html="this.data.second_section_description">
+
                     </p>
+                    <!--                        <span>ARTPRACTICA FURNITURE — </span>это опытная команда профессионалов и высокотехнологичное производство полного цикла. Успешно воплощаем идеи дизайнеров. Индивидуальный подход к каждому проекту.-->
+
                 </div>
                 <div class="row">
                     <div class="equipment__column">
@@ -36,46 +32,24 @@
                             <img
                                 lazy
                                 v-lazy="{
-                                  src: '/images/design/img_4.png',
+                                  src: this.data.second_section_image,
                                   error: 'url',
                                 }"
                                 alt="">
                         </div>
                     </div>
-                    <div class="equipment__column">
-                        <div class="equipment__item">
+                    <div class="equipment__column" v-if="this.data.first_section_description.length > 0">
+                        <div
+                            v-for="(item, idx) in this.data.first_section_description"
+                            class="equipment__item">
                             <div class="equipment__item-title">
                                 <h4>
-                                    <span>01</span>
+                                    <span>0{{idx+1}}</span>
                                 </h4>
                             </div>
                             <div class="equipment__item-text">
                                 <p>
-                                    Группа штатных конструкторов и собственное производство позволяют нам реализовать дизайнерские задачи любой сложности на высоком уровне.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="equipment__item">
-                            <div class="equipment__item-title">
-                                <h4>
-                                    <span>02</span>
-                                </h4>
-                            </div>
-                            <div class="equipment__item-text">
-                                <p>
-                                    Мы создаем мебель, начиная с первичных замеров, заканчивая установкой изделия. На производстве используются материалы и комплектующие, имеющие сертификаты соответствия и безопасности, а сам процесс тщательно контролируется профессионалами.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="equipment__item">
-                            <div class="equipment__item-title">
-                                <h4>
-                                    <span>03</span>
-                                </h4>
-                            </div>
-                            <div class="equipment__item-text">
-                                <p>
-                                    Пристальное внимание к деталям прослеживается на каждом этапе изготовления и реализации изделий.
+                                    {{ item.textarea }}
                                 </p>
                             </div>
                         </div>
@@ -87,37 +61,23 @@
         <div class="atelier-apfurniture">
             <div class="container">
                 <div class="atelier-apfurniture-title service-title">
-                    APFURNITURE.RU
+                    {{this.data.third_section_title}}
                 </div>
                 <div class="atelier-apfurniture-text">
                     <p class="service-text">
-                        Сотрудничаем с дизайнерами по РФ. Опыт и техническое оснащение позволяют качественно реализовать любую вашу идею. С нашими работами вы можете подробно ознакомиться на нашем сайте
+                        {{this.data.third_section_description}}
                     </p>
                 </div>
-                <div class="row">
-                    <div class="atelier-apfurniture-item">
+                <div class="row" v-if="this.data.third_section_images.length > 0">
+
+                    <div
+                        v-for="(item, idx) in this.data.third_section_images"
+                        :key="idx"
+                        class="atelier-apfurniture-item">
                         <img
                             lazy
                             v-lazy="{
-                              src: '/images/design/img_5.png',
-                              error: 'url',
-                            }"
-                            alt="" class="atelier-apfurniture-image">
-                    </div>
-                    <div class="atelier-apfurniture-item">
-                        <img
-                            lazy
-                            v-lazy="{
-                              src: '/images/design/img_6.png',
-                              error: 'url',
-                            }"
-                            alt="" class="atelier-apfurniture-image">
-                    </div>
-                    <div class="atelier-apfurniture-item">
-                        <img
-                            lazy
-                            v-lazy="{
-                              src: '/images/design/img_7.png',
+                              src: item,
                               error: 'url',
                             }"
                             alt="" class="atelier-apfurniture-image">
